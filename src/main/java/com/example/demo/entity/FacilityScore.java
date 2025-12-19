bcf33431f4ca74fdfda635ae168dbb6031c2231b
bala-amypo <bala@amypo.in>
2025-12-19 08:53:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/FacilityScore.java
@@ -0,0 +1,38 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FacilityScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "property_id", nullable = false, unique = true)
+    private Property property;
+
+    @Min(0)
+    @Max(10)
+    private Integer schoolProximity;
+
+    @Min(0)
+    @Max(10)
+    private Integer hospitalProximity;
+
+    @Min(0)
+    @Max(10)
+    private Integer transportAccess;
+
+    @Min(0)
+    @Max(10)
+    private Integer safetyScore;
+}
