788b9b1e4c70c77423531fd177e8832d4686422c
bala-amypo <bala@amypo.in>
2025-12-24 04:20:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,41 +4,19 @@ import jakarta.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(
-    name = "users",
-    uniqueConstraints = @UniqueConstraint(columnNames = "email")
-)
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-
-    @Column(nullable = false)
     private String email;
-
     private String password;
     private String role;
-
-    @ManyToMany
-    @JoinTable(
-        name = "user_properties",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "property_id")
-    )
-    private Set<Property> assignedProperties = new HashSet<>();
-
-    public User() {}
-
-    public User(String name, String email, String password, String role) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    // getters and setters
 }
