a15b0b2784413098f6c3890b0fd795526f75afdf
bala-amypo <bala@amypo.in>
2025-12-29 08:10:44 +0000

    User commit

--- src/main/java/com/example/demo/service/RatingLogService.java
+++ src/main/java/com/example/demo/service/RatingLogService.java
@@ -4,8 +4,6 @@ import com.example.demo.entity.RatingLog;
 import java.util.List;
 
 public interface RatingLogService {
-
     RatingLog addLog(Long propertyId, String message);
-
     List<RatingLog> getLogsByProperty(Long propertyId);
 }
